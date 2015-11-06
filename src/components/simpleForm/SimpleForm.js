09e95da0c3edf5fc074492f6a016a58759600072
dbertella <daniele.bertella@gmail.com>
2015-11-06 14:49:22 +0100

    fix typo in package.json add dumb form component

---
+++ src/components/simpleForm/SimpleForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export default class SimpleForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputs: this.props.inputs || [],
+    }
+  }
+  addOwner(e) {
+    e.preventDefault();
+    const input = this.refs.newInput.value.trim();
+    let inputs = this.state.inputs;
+    if (input) {
+      inputs.push(input);
+      this.setState({
+        inputs: inputs,
+      })
+    }
+    this.refs.newInput.value = '';
+  }
+  render() {
+    const inputs = this.state.inputs.map((el, i) => {
+      return (
+        <li key={i}>{el}</li>
+      );
+    });
+    return (
+      <div>
+        <h3>FOLDER’S inputs</h3>
+        <ul>
+          { inputs }
+        </ul>
+        <form onSubmit={this.addOwner.bind(this)}>
+          <input type="text" ref="newInput" />
+          <button>ADD OWNER</button>
+        </form>
+      </div>
+    );
+  }
+}
